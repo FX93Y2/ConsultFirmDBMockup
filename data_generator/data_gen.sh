1654227caa6f8164987e80bbd2ffcc626b1b9b86
Fy981006 <puttingsalt@icloud.com>
2024-06-09 07:31:04 +0800

    deleted:    data_generator/data_gen.sh 	modified:   data_generator/generate_project.py 	deleted:    notebooks 	deleted:    tests/

--- data_generator/data_gen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Define the number of projects
-NUM_PROJECTS=1000
-
-echo "Generating $NUM_PROJECTS project plans..."
-python3 generate_deliverable.py $NUM_PROJECTS
-
-echo "Generation complete."
--- tests/test_main.py
+++
@@ -1,9 +0,0 @@
-from test_generate_consultant import main as generate_consultant
-
-def main():
-    num_titles = 4000
-    num_years = 10
-    generate_consultant(num_titles, num_years)
-
-if __name__ == "__main__":
-    main()
