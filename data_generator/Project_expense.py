c5d21177249f192667a54d6f82c7f32342291701
rachel11q <ruixuanq@usc.edu>
2024-06-08 22:30:52 +0800

    Project_expense.py

--- data_generator/Project_expense.py
+++ data_generator/Project_expense.py
@@ -12,21 +12,16 @@ def generate_project_expense(num_expenses, project_start_date, project_end_date)
     data_path = os.path.join(base_path, 'data', 'processed')
     csv_file_path = os.path.join(data_path, "Project_Expense.csv")
 
-    # Ensure the data directory exists
     os.makedirs(data_path, exist_ok=True)
 
-    # Initialize Faker
     fake = Faker()
 
-    # Convert string dates to date objects
     project_start_date = datetime.strptime(project_start_date, '%Y-%m-%d').date()
     project_end_date = datetime.strptime(project_end_date, '%Y-%m-%d').date()
 
-    # Predefined expense categories with probabilities
     categories = ['Travel', 'Supplies', 'Equipment', 'Other']
     probabilities = [0.4, 0.3, 0.2, 0.1]
 
-    # Generate expense data
     expense_data = []
     for i in range(num_expenses):
         project_expense_id = i + 1
@@ -39,19 +34,16 @@ def generate_project_expense(num_expenses, project_start_date, project_end_date)
 
         expense_data.append([project_expense_id, project_id, deliverable_id, date, amount, description, category])
 
-    # Create a DataFrame
     expense_df = pd.DataFrame(expense_data,
                               columns=['ProjectExpenseID', 'Project_ID', 'DeliverableID', 'Date', 'Amount',
                                        'Description', 'Category'])
 
-    # If the CSV file already exists, append the new data to it
     if os.path.exists(csv_file_path):
         existing_df = pd.read_csv(csv_file_path)
         combined_df = pd.concat([existing_df, expense_df], ignore_index=True)
     else:
         combined_df = expense_df
 
-    # Save the combined data back to the CSV file
     combined_df.to_csv(csv_file_path, index=False)
     print(f"Data saved to {csv_file_path}")
 
@@ -60,7 +52,6 @@ def main(num_expenses, project_start_date, project_end_date):
 
 
 if __name__ == "__main__":
-    # Example usage
     num_expenses = 100  # Define the number of expenses to generate
     project_start_date = '2023-01-01'  # Define the project start date
     project_end_date = '2023-12-31'  # Define the project end date
