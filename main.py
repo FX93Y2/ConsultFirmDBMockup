c40df6850558537f52e115e30a43b38b0198121c
USCXS <sxu10885@usc.edu>
2024-06-08 20:06:49 -0700

    Combing Proj and Billing Rate generator

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 #from data_generator.generate_consultant import main as generate_consultant
 from data_generator.generate_project import main as generate_project
 from data_generator.Project_Billing_Rate import main as project_Billing_Rate
+from data_generator.generate_proj_and_billing_rate import main as generate_proj_and_billing_rate
+
 
 def main():
     num_titles = 1000
@@ -12,7 +14,8 @@ def main():
     #generate_consultant(num_titles, num_years)
     #generate_client(num_client)
     #generate_project(num_projects)
-    project_Billing_Rate(100, [1, 2, 3, 4, 5])
+    #project_Billing_Rate(100, [1, 2, 3, 4, 5])
+    generate_proj_and_billing_rate(num_projects)
 
 if __name__ == "__main__":
     main()
